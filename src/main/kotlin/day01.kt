20b955d109183339e7e865f7eb4002dba1889044
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2022-01-16 09:46:57 +0100

    refactored day 01 to avoid duplicate code

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -7,31 +7,23 @@ const val HINT_PART2 = "()())" // should result in 55555
 
 fun getRealInput() = File(INPUT_FILE).readLines()[0]
 
-fun getFloor(directions: String): Int {
-    var floor = 0
-    for (char in directions) {
-        when (char) {
-            '(' -> floor += 1
-            ')' -> floor -= 1
-            else -> println("instruction $char is not recognised")
-        }
-    }
-    return floor
-}
+fun getFloor(directions: String) = findFloorAndBasement(directions).first
 
-fun findBasement(directions: String): Int {
+fun findBasement(directions: String) = findFloorAndBasement(directions).second
+
+fun findFloorAndBasement(directions: String): Pair<Int, Int> {
     var floor = 0
+    var basementIndex = -1 // stays negative if basement never reached
     for ((index, char) in directions.withIndex()) {
         when (char) {
             '(' -> floor += 1
             ')' -> floor -= 1
             else -> println("instruction $char is not recognised")
         }
-        if (floor == -1) return index + 1
+        if (floor == -1 && basementIndex < 0) basementIndex = index + 1
     }
-    return -1 // never reached basement
+    return Pair(floor, basementIndex)
 }
-
 
 fun main() {
     println("hint 1 results in ${getFloor(HINT1)}")
