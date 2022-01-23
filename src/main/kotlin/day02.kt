7d5e45a740e3d2eb2bfdd8544730bd6d281ac379
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2022-01-23 12:54:23 +0100

    refactor to have a more functional program

--- src/main/kotlin/day02.kt
+++ src/main/kotlin/day02.kt
@@ -1,25 +1,31 @@
+val pairs = listOf(Pair(0, 1), Pair(0, 2), Pair(1, 2))
+
 class Input {
-    fun input() = this::class.java.classLoader.getResource("input02.txt").readText().trim().split("\n")
+    fun input() = this::class.java.classLoader
+        .getResource("input02.txt")
+        .readText().trim().split("\n")
+        .map { it.interpret() }
 }
 
-fun requiredPaper(dimString: String): Int {
-    val dimensions = dimString.split("x").map { it.toInt() }
-    val areas = listOf(
-        dimensions[0] * dimensions[1],
-        dimensions[0] * dimensions[2],
-        dimensions[1] * dimensions[2],
-    ).sorted()
+fun String.interpret(): List<Int> = this.split("x").map { it.toInt() }
+
+fun List<Int>.requiredPaper(): Int {
+    val areas = pairs.map { this[it.first] * this[it.second] }.sorted()
     return areas[0] * 3 + areas[1] * 2 + areas[2] * 2
+}
+
+fun requiredRibbon(dimString: String): Int {
+    return 0
 }
 
 fun checkHints1() {
     print("Hint1 (should be 58): ")
-    println(requiredPaper("2x3x4"))
+    println("2x3x4".interpret().requiredPaper())
     print("Hint2 (should be 43): ")
-    println(requiredPaper("1x1x10"))
+    println("1x1x10".interpret().requiredPaper())
 }
 
-fun solvePart1() = Input().input().map { requiredPaper(it) }.sum()
+fun solvePart1() = Input().input().map { it.requiredPaper() }.sum()
 
 fun main() {
     println("total paper required: ${solvePart1()}")
