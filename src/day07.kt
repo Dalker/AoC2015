4d936bba06892f4e71e196ace1b54cb67405ec3d
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2022-02-27 23:12:06 +0100

    hacked some funs to get nicer when-regex code

--- src/day07.kt
+++ src/day07.kt
@@ -12,6 +12,13 @@ NOT y -> i
 """
 const val INPUT_FILE = "input/input07.txt"
 
+// following hack taken from https://discuss.kotlinlang.org/t/using-regex-in-a-when/1794/4
+operator fun Regex.contains(text: CharSequence): Boolean = this.matches(text)
+// goal: imitate nice Scala when(string) {regex -> ...} feature
+// plus my own complementary hack below:
+fun <T> Regex.let(text: CharSequence, lambda: (List<String>) -> T) =
+    this.find(text)!!.groupValues.let(lambda)
+
 class Circuit(val logging: Boolean = false) {
     val rules: MutableMap<String, String> = mutableMapOf()
     val values: MutableMap<String, UInt> = mutableMapOf()
@@ -41,29 +48,17 @@ class Circuit(val logging: Boolean = false) {
         if (logging) println("evaluating wire $wire")
         val rule = rules.get(wire)!!
         if (logging) println("must compute $wire with $rule")
-        val result = when {
-            assignPattern.matches(rule) -> valueOf(rule)
-            notPattern.matches(rule) ->
+        val result = when (rule) {
+            in assignPattern -> valueOf(rule)
+            in notPattern ->
                 /* circumvent Kotlin not handling 16-bit bitwise operations... :-( */
-                notPattern.find(rule)!!.groupValues.let {
+                notPattern.let(rule) {
                     (65536u + valueOf(it[1]).inv() - 4294967296u).toUInt()
                 }
-            andPattern.matches(rule) ->
-                andPattern.find(rule)!!.groupValues.let {
-                    valueOf(it[1]) and valueOf(it[2])
-                }
-            orPattern.matches(rule) ->
-                orPattern.find(rule)!!.groupValues.let {
-                    valueOf(it[1]) or valueOf(it[2])
-                }
-            lshiftPattern.matches(rule) ->
-                lshiftPattern.find(rule)!!.groupValues.let {
-                    valueOf(it[1]) shl it[2].toInt()
-                }
-            rshiftPattern.matches(rule) ->
-                rshiftPattern.find(rule)!!.groupValues.let {
-                    valueOf(it[1]) shr it[2].toInt()
-                }
+            in andPattern -> andPattern.let(rule) { valueOf(it[1]) and valueOf(it[2]) }
+            in orPattern -> orPattern.let(rule) { valueOf(it[1]) or valueOf(it[2]) }
+            in lshiftPattern -> lshiftPattern.let(rule) { valueOf(it[1]) shl it[2].toInt() }
+            in rshiftPattern -> rshiftPattern.let(rule) { valueOf(it[1]) shr it[2].toInt() }
             else -> 0u.also {
                 println("wire $wire with rule $rule couldn't be interpreted")
             }
